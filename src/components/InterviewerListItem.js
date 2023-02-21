1d0904881ce2d5e696470c1c6d9b54addd7c5749
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-20 19:16:16 -0700

    Create initial code and refactor

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+
+  const intItemClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  const clickHandler = () => {
+    if (props.setInterviewer) {
+      props.setInterviewer();
+    }
+  }
+
+  return (
+    <li className={intItemClass} onClick={clickHandler}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
