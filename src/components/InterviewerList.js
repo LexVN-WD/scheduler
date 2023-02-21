675cde76a2b302372a2cce993455b5cf693efc36
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-20 19:28:59 -0700

    Refactor code with map

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,11 +2,37 @@ import React from "react";
 
 import "components/InterviewerList.scss";
 
+import InterviewerListItem from "./InterviewerListItem";
+
 export default function InterviewerList(props) {
+
+  const intArr = props.interviewers.map(i => {
+    if (props.setInterviewer) {
+      return (
+        <InterviewerListItem
+          key={i.id}
+          name={i.name}
+          avatar={i.avatar}
+          selected={i.id === props.interviewer}
+          setInterviewer={() => props.setInterviewer(i.id)}
+        />
+      );
+    } else {
+      return (
+        <InterviewerListItem
+          key={i.id}
+          name={i.name}
+          avatar={i.avatar}
+          selected={i.id === props.interviewer}
+        />
+      );
+    }
+  })
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{intArr}</ul>
     </section>
   );
 }
