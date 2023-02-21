11352b0ed25b6dfe653df815fd9996f4f1317b22
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-20 21:10:44 -0700

    Refactor to show Schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,8 +4,10 @@ import "components/Application.scss";
 
 import DayList from "./DayList";
 
+import "components/Appointment"
+import Appointment from "components/Appointment";
 
-
+// Mock Data
 const days = [
   {
     id: 1,
@@ -24,9 +26,61 @@ const days = [
   },
 ]
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+
+  const appointmentsArr = Object.values(appointments).map(i => {
+    return (
+      <Appointment
+        key={i.id}
+        id={i.id}
+        time={i.time}
+        interview={i.interview}
+      />
+    )
+  })
+
+  const appointmentPropsArr = [...appointmentsArr, <Appointment key="last" time="5pm" />];
 
   return (
     <main className="layout">
@@ -51,7 +105,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentPropsArr}
       </section>
     </main>
   );
