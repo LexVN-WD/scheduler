69561aaae5122b6055a41335f3e1e1062419f908
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-20 20:09:49 -0700

    Import Show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "components/Appointment/styles.scss";
 
 import Header from "./Header";
+import Show from "./Show";
 
 export default function Appointment(props) {
   return (
