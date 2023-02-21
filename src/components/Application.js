64aa9835a2f4987711798a429c4ff6111e552c87
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-20 19:00:00 -0700

    Refactor code to include useState

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "components/Application.scss";
 
@@ -25,6 +25,9 @@ const days = [
 ]
 
 export default function Application(props) {
+
+  const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -37,8 +40,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
