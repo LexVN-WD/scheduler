1b35994a29388894de7d09cb3e85282115645ebc
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-24 15:43:07 -0700

    Refactor using Promise.all to retrieve appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,46 +11,8 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment"
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
 
 export default function Application(props) {
 
@@ -60,18 +22,23 @@ export default function Application(props) {
     appointments: {}
   });
 
+  const dailyAppointments = [];
+
   const setDay = day => setState({ ...state, day });
-  const setDays = (days) => setState(prev => ({ ...prev, days }));
 
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days')
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ])
     .then((response) => {
-      setDays(response.data);
+      setState(prev => ({...prev, days: response[0].data, appointments: response[1].data}));
     })
   }, [])
 
-  const appointmentsArr = Object.values(appointments).map(i => {
+  const appointmentsArr = getAppointmentsForDay(state, state.day).map(i => {
     return (
       <Appointment
         key={i.id}
