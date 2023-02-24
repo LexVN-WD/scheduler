0fa1396be6c2c4878f4e55cb74355b04386556b7
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-24 16:12:38 -0700

    Import getInterview helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,7 +11,7 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import "components/Appointment"
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
