7d3de04c46de35e36f6ed09efd82ece8f151f964
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-25 17:39:55 -0700

    Create custom hook for change state logic

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+export default function useApplicationData() {
+  
+  // state Object
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+  
+  // setDay action
+  const setDay = day => setState({ ...state, day });
+  
+  // Retrieve data from API
+  useEffect(() => {
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ])
+      .then((response) => {
+        setState(prev => ({ ...prev, days: response[0].data, appointments: response[1].data, interviewers: response[2].data }));
+      })
+  }, []);
+  
+  // bookInterview Function
+  function bookInterview(id, interview) {
+    const appointment = { ...state.appointments[id], interview: { ...interview } };
+    const appointments = { ...state.appointments, [id]: appointment };
+    return axios.put(`http://localhost:8001/api/appointments/${id}`, { interview })
+      .then(() => setState({ ...state, appointments }));
+  };
+  
+  // cancelInterview Function
+  function cancelInterview(id) {
+    const appointment = { ...state.appointments[id], interview: null };
+    const appointments = { ...state.appointments, [id]: appointment };
+    return axios.delete(`http://localhost:8001/api/appointments/${id}`)
+      .then(() => setState({ ...state, appointments }));
+  };
+
+  return { state, setDay, bookInterview, cancelInterview }
+
+};
+
+
