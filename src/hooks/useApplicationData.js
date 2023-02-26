df7d852df424ee4b44cb8fe4f1454abf0b59fc56
Alexander <apvannug.lhl.webdev@gmail.com>
2023-02-25 19:29:12 -0700

    Refactor useEffect function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -18,13 +18,17 @@ export default function useApplicationData() {
   // Retrieve data from API
   useEffect(() => {
     Promise.all([
-      axios.get('http://localhost:8001/api/days'),
-      axios.get('http://localhost:8001/api/appointments'),
-      axios.get('http://localhost:8001/api/interviewers')
-    ])
-      .then((response) => {
-        setState(prev => ({ ...prev, days: response[0].data, appointments: response[1].data, interviewers: response[2].data }));
-      })
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
   }, []);
   
   // bookInterview Function
